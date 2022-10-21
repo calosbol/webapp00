bfaf935304f764fededf2360fe10449cffa9554b
calosbol <116382767+calosbol@users.noreply.github.com>
2022-10-21 16:05:59 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,7 +10,7 @@ st.title("SENAI ZERBINI É NOTA 1000!")
 st.header("abacaxi em tudo!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("bem vindo!")
+st.subheader("hay")
 
-# Use st.write("") para adicionar um texto ao seu Web app
+# Use st.write("limon") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
