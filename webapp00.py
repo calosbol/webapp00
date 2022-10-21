f63388b2753f641772ff318dec145717187bcc1e
calosbol <116382767+calosbol@users.noreply.github.com>
2022-10-21 16:21:46 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("eu sou NOTA 1000!")
+st.title("kjmc modas!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("fogo neles!")
+st.header("are fashion!")
 
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("melhor q todos")
+# Use st.subheader("click abaixo para mais informaçoes") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("")
 
 # Use st.write("limon") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
