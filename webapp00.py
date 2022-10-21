142da53657e4dd99b1ec412d80a9b4a8997f39ee
calosbol <116382767+calosbol@users.noreply.github.com>
2022-10-21 16:22:50 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -9,8 +9,8 @@ st.title("kjmc modas!")
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("are fashion!")
 
-# Use st.subheader("click abaixo para mais informaçoes") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("")
+# Use st.subheader(") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("click abaixo para mais informaçoes")
 
 # Use st.write("limon") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
